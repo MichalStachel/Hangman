31eb4df508ad83e3da8500cafa9b38647a57653c
MichalStachel <41749093+MichalStachel@users.noreply.github.com>
2020-05-31 16:27:42 +0200

    Updates

---
+++ precache-manifest.9d4a4a1703b836306eac0a67fcd13e63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a070ee61f9bcdc893594e8e15d1f79",
+    "url": "/Hangman/index.html"
+  },
+  {
+    "revision": "f50527163d4563f641dd",
+    "url": "/Hangman/static/css/main.1718688d.chunk.css"
+  },
+  {
+    "revision": "14032058825889919b47",
+    "url": "/Hangman/static/js/2.46e4ea8f.chunk.js"
+  },
+  {
+    "revision": "f50527163d4563f641dd",
+    "url": "/Hangman/static/js/main.fa579e10.chunk.js"
+  },
+  {
+    "revision": "f8832925578ad51d01a6",
+    "url": "/Hangman/static/js/runtime~main.9bccb078.js"
+  },
+  {
+    "revision": "984538bfeed034d507caa10cc78b043f",
+    "url": "/Hangman/static/media/hangman.984538bf.png"
+  },
+  {
+    "revision": "758458275a2fb070ede716db94d7413c",
+    "url": "/Hangman/static/media/hangman8.75845827.png"
+  }
+]);
